d4e29ef84ee5e6043dcd9bb2c4e5d05162e1e2e6
Adrian Lindberg <adrlin@student.chalmers.se>
2018-02-25 15:46:04 +0100

    Added endpoints for amount in cart

--- react-backend/routes/cart.js
+++ react-backend/routes/cart.js
@@ -5,12 +5,16 @@ router.get('/:userid', function(req, res, next) {
   res.json({
     id: 1,
     total_price: 100,
+    total_amount: 3,
     items_id: [{
-      id: 1
+      id: 1,
+      amount: 1
     }, {
-      id: 2
+      id: 2,
+      amount: 1
     }, {
-      id: 3
+      id: 3,
+      amount: 1
     }]
   });
 });
@@ -18,6 +22,12 @@ router.get('/:userid', function(req, res, next) {
 router.get('/:userid/total_price', function(req, res, next) {
   res.json({
     total_price: 100
+  });
+});
+
+router.get('/:userid/total_amount', function(req, res, next) {
+  res.json({
+    total_amount: 3
   });
 });
 
