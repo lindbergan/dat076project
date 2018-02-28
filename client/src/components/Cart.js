c39a8771212269f76223bdce0eb6317a714426c3
Jonathan Sundkvist <jonran@student.chalmers.se>
2018-02-28 16:21:11 +0100

    grids in Header.js, fetching from cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
-function handleClick(e){
-}
 
 export class Cart extends Component{
 
@@ -14,95 +11,25 @@ export class Cart extends Component{
   }
 
   async componentDidMount(){
+    console.log("mounting successful");
     fetch('cart/1')
       .then(res => res.json())
       .then(cart => this.setState({cart}));
   }
-
-
   render(){
 
-    const cart = this.state.cart;
-    if(!cart){
+    const total_price = this.state.cart.total_price;
+    console.log(total_price);
+    if(total_price === 0){
       return(
 
-        <div className="cart-container">Cart is empty
-
-
-        <style jsx="true">{`
-
-          .cart-container {
-            background-color: steelblue;
-            display:grid;
-            grid-template-columns: 50% 50%;
-            grid-template-rows: 15% 60% 15%;
-            grid-template-areas:
-              ". . ."
-              "grid-icon grid-cart-info"
-              ". . ."
-          }
-
-        `}</style>
-        </div>
+        <div>Cart is empty</div>
 
     );
     }else{
-      const total_amount = cart.total_amount;
-      const total_price = cart.total_price;
       return(
 
-        <div className="cart-container">
-          <div className="grid-icon">
-            <div className="cart-icon">
-              <i className="fas fa-shopping-cart"></i>
-            </div>
-          </div>
-          <div className="grid-cart-info">
-            <div>Number of products: {total_amount}</div>
-            <div>Total cost: {total_price}</div>
-            <div className="purchase-btn">
-              <Link to="/checkout" onClick={handleClick}>Go to cashier</Link>
-            </div>
-          </div>
-
-        <style jsx="true">{`
-
-
-          .cart-container {
-            color: white;
-            background-color: steelblue;
-            display:grid;
-            grid-template-columns: 50% 50%;
-            grid-template-rows: 15% 60% 15%;
-            grid-template-areas:
-              ". . "
-              "grid-icon grid-cart-info"
-              ". ."
-          }
-          .grid-icon{
-            display: grid;
-            grid-area: grid-icon;
-            text-align: center;
-            content-align:center;
-          }
-          .cart-icon{
-            font-size:4em;
-            color:white;
-          }
-          .grid-cart-info{
-            display: grid;
-            grid-area: grid-cart-info;
-            margin-left: 0;
-            text-align: left;
-          }
-          .purchase-btn a{
-            color: white;
-            cursor: pointer;
-          }
-
-        `}</style>
-
-        </div>
+        <div>Products exists</div>
 
       );
     }
