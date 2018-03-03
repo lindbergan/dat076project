7a000c9e083c9eeb8b764dbe4fc0824df029f134
Jonathan Sundkvist <jonran@student.chalmers.se>
2018-03-03 13:14:23 +0100

    Refactored sidebar and header into Layout.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from './Layout.js';
 import { Header } from './components/header.js';
 import { Sidebar } from './components/sidebar.js';
 import { GridView } from "./components/gridview";
@@ -25,48 +26,9 @@ class App extends Component {
       }/>
     }
     return (
-      <div className="App grid-container">
-        <div className="grid-header">
-          <Header/>
-        </div>
-        <div className="grid-sidebar">
-          <Sidebar/>
-        </div>
-        <div className="grid-main">
-          <GridView />
-        </div>
-        <style jsx="true">{`
-          .grid-container{
-            display:grid;
-            grid-template-rows: 150px auto;
-            grid-template-columns: 20% 80%;
-            grid-template-areas:
-              "grid-header grid-header"
-              "grid-sidebar grid-main"
-          }
-          .grid-header{
-            display:grid;
-            grid-area: grid-header;
-            height: 100%;
-            width:100%;
-            background-color: #B5C7CB;
-          }
-
-          .grid-sidebar{
-            display: grid;
-            grid-area: grid-sidebar;
-            background-color: #EAEAEA;
-          }
-
-          .grid-main{
-            display: grid;
-            grid-area: grid-main;
-            background-color: #EAEAEA;
-            height: 100%;
-            width: 100%;
-          }
-        `}</style>
-      </div>
+      <Layout>
+        <GridView />
+      </Layout>
     );
   }
 }
