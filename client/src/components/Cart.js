531a6c76e87fab520e5bad1a2e6a9e7a22eb9347
Adrian Lindberg <adrlin@student.chalmers.se>
2018-03-07 13:56:48 +0100

    Removed cart is empty

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -27,67 +27,47 @@ export class Cart extends Component{
   render() {
 
     const cart = this.state.cart;
-    if(cart.length === 0){
-      return(
+    const total_amount = cart.length;
+    return(
 
-        <div className="cart-container">Cart is empty
+      <Grid className="cart-container" fluid={true}>
+        <Row className="grid-icon">
+          <div className="cart-icon">
+            <MaterialIcon icon="shopping_cart" invert={true} size={50}/>
+          </div>
+        </Row>
+        <Row className="grid-cart-info">
+          <div>Number of products: {total_amount}</div>
+          <div>Total cost: {total_amount}</div>
+          <div className="purchase-btn">
+            <Link to="/checkout" >Go to cashier</Link>
+          </div>
+        </Row>
+
+      <style jsx="true">{`
 
 
-        <style jsx="true">{`
+        .cart-container {
+          color: white;
+          background-color: steelblue;
+        }
+        .grid-icon{
+        }
+        .cart-icon{
+          font-size:4em;
+          color:white;
+        }
+        .grid-cart-info{
+        }
+        .purchase-btn a{
+          color: white;
+          cursor: pointer;
+        }
 
-          .cart-container {
-            background-color: steelblue;
-          }
+      `}</style>
 
-        `}</style>
-        </div>
+      </Grid>
 
     );
-    }else{
-      const total_amount = cart.length;
-      return(
-
-        <Grid className="cart-container" fluid={true}>
-          <Row className="grid-icon">
-            <div className="cart-icon">
-              <MaterialIcon icon="shopping_cart" invert={true} size={50}/>
-            </div>
-          </Row>
-          <Row className="grid-cart-info">
-            <div>Number of products: {total_amount}</div>
-            <div>Total cost: {total_amount}</div>
-            <div className="purchase-btn">
-              <Link to="/checkout" >Go to cashier</Link>
-            </div>
-          </Row>
-
-        <style jsx="true">{`
-
-
-          .cart-container {
-            color: white;
-            background-color: steelblue;
-          }
-          .grid-icon{
-          }
-          .cart-icon{
-            font-size:4em;
-            color:white;
-          }
-          .grid-cart-info{
-          }
-          .purchase-btn a{
-            color: white;
-            cursor: pointer;
-          }
-
-        `}</style>
-
-        </Grid>
-
-      );
-    }
-
   }
-
 }
