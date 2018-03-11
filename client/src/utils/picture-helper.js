5f37d4c1e2e43b659b898e68374956e84012e25e
Adrian Lindberg <adrlin@student.chalmers.se>
2018-03-11 15:09:09 +0100

    Fixed issue with picture-helper

--- client/src/utils/picture-helper.js
+++ client/src/utils/picture-helper.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PictureHelper = {
 
-  renderImage(product) {
+  /*renderImage(product) {
     if (product !== '') {
       return (<img src={require('../img/' + product.product_id + '.png')} alt="product" height="100%" width="100%"
       content="fit" align="center"/>)
     }
-  }
+  }*/
 
 };
 
